608169a8c7104fb79f4f2c6784bff151dff1e0d1
motasimmakki <makkimotasim@gmail.com>
2022-10-27 20:44:52 +0530

    :tada:feat: Added animation on record and capture button

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // alert("Working . . .");
+let recordBtn = document.querySelector(".record-circle");
+let captureBtn = document.querySelector(".capture-circle");
+let timerBox = document.querySelector(".timer-box p");
+
+let isRecording = false;
+recordBtn.addEventListener("click", function() {
+    if(!isRecording) {
+        recordBtn.classList.add("scale-record");
+        timerBox.style.visibility = "visible";
+    } else {
+        recordBtn.classList.remove("scale-record");
+        timerBox.style.visibility = "hidden";
+    }
+    isRecording = !isRecording;
+});
+
+captureBtn.addEventListener("click", function() {
+    if(!isRecording) {
+        captureBtn.classList.add("scale-capture");
+        setTimeout(() => {
+            captureBtn.classList.remove("scale-capture");
+        }, 1000);
+    }
+});
