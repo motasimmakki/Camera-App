cc917848d616998ec0e4452fee8e4768b8a024dd
motasimmakki <makkimotasim@gmail.com>
2022-10-28 16:05:25 +0530

    :technologist:feat: Made timer functional

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         let blob = new Blob(chunks, {type: "video/mp4"});
         let videoURL = URL.createObjectURL(blob);
         console.log(videoURL);
-
         // Getting recorded video.
         let a = document.createElement("a");
         a.href = videoURL;
@@ -41,13 +40,40 @@ recordBtn.addEventListener("click", function() {
         mediaRecorder.start();
         recordBtn.classList.add("scale-record");
         timerBox.style.visibility = "visible";
+        startTimer()
     } else {
         mediaRecorder.stop();
         recordBtn.classList.remove("scale-record");
         timerBox.style.visibility = "hidden";
+        stopTimer();
     }
     isRecording = !isRecording;
 });
+
+function startTimer() {
+    function displayTimer() {
+        let totalSeconds = counter;
+
+        let hours = parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600;
+        let mins = parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60;
+        let secs = totalSeconds;
+
+        hours = (hours < 10)? `0${hours}`: hours;
+        mins = (mins < 10)? `0${mins}`: mins;
+        secs = (secs < 10)? `0${secs}`: secs;
+
+        timerBox.innerText = `${hours}:${mins}:${secs}`;
+        counter += 1;
+    };
+    setInterval(displayTimer, 1000);
+    counter = 1;
+}
+
+function stopTimer() {
+    
+}
 
 captureBtn.addEventListener("click", function() {
     if(!isRecording) {
