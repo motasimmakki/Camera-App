a7c9e220b30144b03ee8463deab4ac554820f4a4
motasimmakki <makkimotasim@gmail.com>
2022-10-31 12:17:00 +0530

    :technologist:feat: Added video view on gallery

--- galley.js
+++ galley.js
@@ -6,15 +6,15 @@ backBtn.addEventListener("click", function() {
 
 setTimeout(() => {
     if(db) {
+        let galleryCont = document.querySelector(".media-cont-box");
         let imageDBTransaction = db.transaction("image", "readonly");
         let imageStore = imageDBTransaction.objectStore("image");
         let imageRequest = imageStore.getAll();
         imageRequest.onsuccess = function() {
             if(imageRequest.result !== undefined) {
                 // imageRequest.result -> returns the objects of all images in an array.
-                console.log("Images", imageRequest.result);
+                // console.log("Images", imageRequest.result);
                 let imageResult = imageRequest.result;
-                let galleryCont = document.querySelector(".media-cont-box");
                 imageResult.forEach((imageObj) => {
                     let url = imageObj.url;
                     // Create a image container.
@@ -33,6 +33,31 @@ setTimeout(() => {
                 });
             } else {
                 console.log("No Image Found!");
+            }
+        }
+
+        let videoDBTransaction = db.transaction("video", "readonly");
+        let videoStore = videoDBTransaction.objectStore("video");
+        let videoRequest = videoStore.getAll();
+        videoRequest.onsuccess = () => {
+            if(videoRequest.result !== undefined) {
+                let videoResult = videoRequest.result;
+                videoResult.forEach((videoObj) => {
+                    let videoEle  = document.createElement("div");
+                    videoEle.setAttribute("class", "media-cont");
+                    let url = URL.createObjectURL(videoObj.blobData);
+                    videoEle.innerHTML = `
+                        <div class="media">
+                            <video autoplay loop src="${url}">
+                        </div>
+                        <div class="delete action-btn">DELETE</div>
+                        <div class="download action-btn">DOWNLOAD</div>
+                    `;
+                    // Append child in gallery container.
+                    galleryCont.appendChild(videoEle);
+                })
+            } else {
+                console.log("No Video Found!");
             }
         }
     }
